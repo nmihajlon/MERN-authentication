080fa5b253d873ac968ab28b4a946bedd59ad4c7
Mihajlo <nmihajlon@gmail.com>
2025-04-10 19:48:32 +0200

    BE: auth controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,97 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password)
+    return res.json({ success: false, message: "Missing Details" });
+
+  try {
+    const existingUser = await User.findOne({ email: email });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    if (existingUser) {
+      res.json({ success: false, message: "User already exists" });
+    }
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      // kada je dev kuki moze da se salje i kroz http
+      secure: process.env.NODE_ENV === "production" ? true : false,
+      // kuki radi samo u istom domenu kada je dev
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Email and password are required",
+    });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production" ? true : false,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production" ? true : false,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+
+    return res.json({ success: true, message: "Logged out" });
+  } catch (error) {}
+};
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,40 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    
-})
+  name: {
+    type: String,
+    requred: true,
+  },
+  email: {
+    type: String,
+    requred: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    requred: true,
+  },
+  verifyOtp: { // 6-cifreni kod
+    type: String,
+    default: '',
+  },
+  verifyOtpExpireAt: { // timestamp u milisekundama
+    type: Number, 
+    default: 0
+  },
+  isAccountVerified: { // korisnik verifikovao nalog putem mejla
+    type: Boolean,
+    default: false
+  },
+  resetOtp: { // OTP kod koji se koristi kad korisnik hoće da resetuje lozinku
+    type: String,
+    default: ''
+  },
+  resetOtpExpireAt: {
+    type: Number, 
+    default: 0
+  }
+});
+
+const User = mongoose.models.user || mongoose.model("User", userSchema);
+export default User;
